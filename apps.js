90e495fc15b38de7ec565139c4960483a9ecbcc6
Santiago Sanchez <santiago.sanchez.git@gmail.com>
2026-09-14 19:50:04 -0300

    estilos de paleta

--- apps.js
+++ apps.js
@@ -1,30 +1,35 @@
 const boton = document.querySelector("#generar");
 const paleta = document.querySelector("#paleta");
+const selectorCantidad = document.querySelector("#cantidad");
 
-boton.addEventListener("click", generarColor);
+boton.addEventListener("click", generarPaleta);
 
+function generarPaleta() {
+    // Limpia la paleta anterior 
+    paleta.innerHTML = "";
+
+    const cantidad = Number(selectorCantidad.value);
+
+    for (let i = 0; i < cantidad; i++) {
+        const color = generarColor();
+        
+        const tarjeta = document.createElement("div");
+        tarjeta.classList.add("color-card");
+        tarjeta.textContent = color;
+        tarjeta.style.backgroundColor = color;
+        
+        paleta.appendChild(tarjeta);
+    }
+}
 
 function generarColor() {
-
     const caracteres = "0123456789ABCDEF";
+    let color = "#";
 
-let color = "#";
+    for (let i = 0; i < 6; i++) {
+        const numero = Math.floor(Math.random() * 16);
+        color += caracteres[numero];
+    }
 
-for (let i = 0; i < 6; i++) {
-
-    const numero = Math.floor(Math.random() * 16);
-
-    color += caracteres[numero];
+    return color;
 }
-
-    const tarjeta = document.createElement("div");
-
-    tarjeta.textContent = color;
-    tarjeta.style.backgroundColor = color;
-    
-    paleta.appendChild(tarjeta);
-
-}
-
-
-
